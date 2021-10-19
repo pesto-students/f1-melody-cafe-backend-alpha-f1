6d5120a9404d19f353c07bdbbbc0b76fb784b812
Manish Ukirade <mukirade@intelinotion.com>
2021-10-19 14:41:45 +0530

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan')
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const usersRouter = require('./routes/users');
 const albumRouter = require('./routes/album');
@@ -10,7 +11,7 @@ const albumTrackRouter = require('./routes/albumTrack');
 const app = express();
 
 //const {winstonLogger} = require('./services/serviceIntiaters');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
