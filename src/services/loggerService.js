221ab1bb6da32546dc8790bbf5f902653eedc67c
Manish Ukirade <mukirade@intelinotion.com>
2021-10-22 18:55:11 +0530

    change the logs

--- src/services/loggerService.js
+++ src/services/loggerService.js
@@ -3,10 +3,9 @@ const winston = require('winston');
 const winstonLogger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+  defaultMeta: { service: 'web-api' },
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
+    new winston.transports.Console()
   ]
 });
 
