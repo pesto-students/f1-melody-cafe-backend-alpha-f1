e740e93fcc002abbb6044174c6323e7903f5a6c9
Manish Ukirade <mukirade@intelinotion.com>
2021-10-28 19:15:06 +0530

    add user get route

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,7 @@ const userController = require('../controller/userController');
 /* GET users listing. */
 //router.get('/:id', userController.getUserById);
 router.post('/',userController.addUser);
+router.get('/:id',userController.getUserById)
 router.put('/:id',userController.editUser);
 
 module.exports = router;
